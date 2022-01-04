df8fd401ef45c559cc7af8d24ecc2fbcad329798
Jameson Andrade <jameson_andrade@hotmail.com.br>
2022-01-04 16:32:15 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,31 @@ const { v4: uuidv4 } = require ('uuid');
 const app = express();
 app.use(express.json());
 
-const custumers = [];
+const customers = [];
 
+function verifyExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find((findcpf) => findcpf.cpf === cpf);
+
+
+    if (!customer){
+        return res.status(400).json({error: "Customer not Found!"});
+    }
+    req.customer = customer;
+    return next();
+}
 
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
     
-    const custumerAlreadyExists = custumers.some((custumer) => custumer.cpf === cpf);
-    if (custumerAlreadyExists){
-        return res.status(400).json({error: "Custumer Already Exists!"});
+    const customer = customers.some((customer) => customer.cpf === cpf);
+
+    if (customer){
+        return res.status(400).json({error: "Customer Already Exists!"});
     }
     
-    custumers.push({
+    customers.push({
         cpf,
         name,
         id: uuidv4(),
@@ -26,17 +39,30 @@ app.post("/account", (req, res) => {
 
 });
 
-app.get("/statement/", (req, res) =>{
-    const { cpf } = req.headers;
 
-    const customer = custumers.find(customer => customer.cpf === cpf);
+app.use(verifyExistsAccountCPF);
 
-    if(!customer){
-        return res.status(400).json({error: "Customer not Found!"});
-    }
-
+app.get("/statement/", (req, res) => {
+   const { customer } = req; 
     return res.status(200).json(customer.statement);
 
+});
+
+app.post("/deposit", (req, res) => {
+    const { description, amount } = req.body;
+
+    const { customer } = req;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send(statementOperation);
 });
 
 app.listen(3333);  
